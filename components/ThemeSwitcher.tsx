bc7af3e339285e6ac1b2614b669cf400f457839b
unknown <buyndelgertuguldur@gmail.com>
2025-04-11 18:09:12 +0800

    first commit

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -3,10 +3,18 @@
 import { useTheme } from "../app/ThemeContext";
 import { CSSProperties } from "react";
 
+// ThemeColor төрлийг ThemeContext-ээс импортлох эсвэл энд тодорхойлох
+type ThemeColor = 'theme-orange' | 'theme-blue' | 'theme-purple' | 'theme-green' | 'theme-red';
+
+interface Color {
+  name: ThemeColor; // ThemeColor төрлийг ашиглана
+  value: string;
+}
+
 export default function ThemeSwitcher() {
   const { themeColor, setThemeColor } = useTheme();
 
-  const colors = [
+  const colors: Color[] = [
     { name: "theme-orange", value: "#f97316" },
     { name: "theme-blue", value: "#0000ff" },
     { name: "theme-purple", value: "#ff00ff" },
@@ -23,7 +31,7 @@ export default function ThemeSwitcher() {
           className={`w-6 h-6 rounded-full border-2 ${
             themeColor === color.name ? "border-white" : "border-transparent"
           }`}
-          onClick={() => setThemeColor(color.name)} 
+          onClick={() => setThemeColor(color.name)} // Одоо төрлийн алдаа гарахгүй
         />
       ))}
     </div>
